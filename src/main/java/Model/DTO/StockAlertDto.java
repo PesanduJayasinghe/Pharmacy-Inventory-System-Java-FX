7a17b654c60101343769c7a49f14070e91bda527
PesanduJayasinghe <pesanduse@gmail.com>
2025-11-29 23:41:57 +0530

    Created StockAlertDto

---
+++ src/main/java/Model/DTO/StockAlertDto.java
@@ -0,0 +1,21 @@
+package Model.DTO;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class StockAlertDto {
+
+    private int medicineId;
+    private String medicineName;
+    private String alertType; // "LOW_STOCK" or "EXPIRY"
+    private String message;   // e.g. "Paracetamol expires in 5 days"
+    private Date expiryDate;
+    private int quantity;
+
+}
