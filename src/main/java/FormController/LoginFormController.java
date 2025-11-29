2e915cbf329008fe7601738b2371bb02e52e465a
PesanduJayasinghe <pesanduse@gmail.com>
2025-11-29 23:26:36 +0530

    Implemented Login from controller

---
+++ src/main/java/FormController/LoginFormController.java
@@ -0,0 +1,118 @@
+package FormController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javax.swing.*;
+
+
+public class LoginFormController {
+
+    private AnchorPane currentPane;
+    private TextField username;
+    private TextField password;
+
+
+    @FXML
+    private AnchorPane adminPane;
+
+    @FXML
+    private AnchorPane btnLoginOption;
+
+    @FXML
+    private AnchorPane cashierPane;
+
+    @FXML
+    private AnchorPane loginOptionsPane;
+
+    @FXML
+    private AnchorPane errorMessagePane;
+
+    @FXML
+    private TextField txtAdminPassword;
+
+    @FXML
+    private TextField txtAdminUsername;
+
+    @FXML
+    private TextField txtCashierPassword;
+
+    @FXML
+    private TextField txtCashierUsername;
+
+    @FXML
+    void btnAdmin(ActionEvent event) {
+        loginOptionsPane.setVisible(false);
+        adminPane.setVisible(true);
+        username=txtAdminUsername;
+        password=txtAdminPassword;
+    }
+
+    @FXML
+    void btnAdminBack(ActionEvent event) {
+        adminPane.setVisible(false);
+        loginOptionsPane.setVisible(true);
+        username.clear();
+        password.clear();
+    }
+
+    @FXML
+    void btnAdminLogin(ActionEvent event) {
+        if (txtAdminUsername.getText().equalsIgnoreCase("owner") && txtAdminPassword.getText().equalsIgnoreCase("1234")){
+
+        }else{
+            errorMessagePane.setVisible(true);
+            adminPane.setVisible(false);
+            currentPane=adminPane;
+        }
+    }
+
+    @FXML
+    void btnCashier(ActionEvent event) {
+        loginOptionsPane.setVisible(false);
+        cashierPane.setVisible(true);
+        username=txtCashierUsername;
+        password=txtCashierPassword;
+
+    }
+
+    @FXML
+    void btnCashierBack(ActionEvent event) {
+        cashierPane.setVisible(false);
+        loginOptionsPane.setVisible(true);
+        username.clear();
+        password.clear();
+    }
+
+    @FXML
+    void btnCashierLogin(ActionEvent event) {
+        if (txtCashierUsername.getText().equalsIgnoreCase("user") && txtCashierPassword.getText().equalsIgnoreCase("1234")){
+
+        }else{
+            errorMessagePane.setVisible(true);
+            cashierPane.setVisible(false);
+            currentPane=cashierPane;
+        }
+
+    }
+
+    //----------ERROR MESSAGE---------
+
+    @FXML
+    void btnMenu(ActionEvent event) {
+        errorMessagePane.setVisible(false);
+        loginOptionsPane.setVisible(true);
+        username.clear();
+        password.clear();
+    }
+
+    @FXML
+    void btnRetry(ActionEvent event) {
+        errorMessagePane.setVisible(false);
+        username.clear();
+        password.clear();
+        currentPane.setVisible(true);
+    }
+
+}
