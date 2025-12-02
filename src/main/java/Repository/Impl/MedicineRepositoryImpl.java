356531166b44e3de0c67a10a52f55f21d307921c
PesanduJayasinghe <pesanduse@gmail.com>
2025-12-03 01:31:26 +0530

    Implemented a new method to repository

--- src/main/java/Repository/Impl/MedicineRepositoryImpl.java
+++ src/main/java/Repository/Impl/MedicineRepositoryImpl.java
@@ -2,9 +2,20 @@ package Repository.Impl;
 
 import Config.HibernateUtil;
 import Model.DTO.MedicineDto;
+import Model.DTO.MedicineSale;
 import Repository.MedicineRepository;
+import db.DB;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicineRepositoryImpl implements MedicineRepository{
 
@@ -16,4 +27,51 @@ public class MedicineRepositoryImpl implements MedicineRepository{
         session.persist(medicineDto);
         transaction.commit();
     }
+
+    @Override
+    public ObservableList<MedicineDto> getMedicineDetails() {
+        return FXCollections.observableArrayList(
+                session.createQuery("FROM MedicineDto", MedicineDto.class).list());
+    }
+
+    @Override
+    public void updateMedicine(MedicineDto medicineDto) {
+        Transaction transaction= session.beginTransaction();
+        session.merge(medicineDto);
+        transaction.commit();
+}
+
+    @Override
+    public void deleteMedicine(String id) {
+        Transaction transaction= session.beginTransaction();
+        session.remove(session.find(MedicineDto.class,id));
+        transaction.commit();
+    }
+
+    @Override
+    public List<MedicineSale> getMedicineChoiceList() {
+            List<MedicineSale> list = new ArrayList<>();
+
+            String sql = "SELECT medicineId, name, price, quantity FROM medicines";
+
+            try (Connection con = DB.getInstance().getConnection();
+                 PreparedStatement ps = con.prepareStatement(sql);
+                 ResultSet rs = ps.executeQuery()) {
+
+                while (rs.next()) {
+                    list.add(new MedicineSale(
+                            rs.getString("medicineId"),
+                            rs.getString("name"),
+                            rs.getInt("quantity"),
+                            rs.getDouble("price")
+                    ));
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        return list;
+    }
+
+
 }
